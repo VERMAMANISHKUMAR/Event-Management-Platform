5d1a9a31943b2cd957d4a8fc9f37f3294a0c2a94
VERMAMANISHKUMAR <manishkumarverma091@gmail.com>
2025-02-02 20:15:03 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3800;
 
 // ✅ CORS Configuration
 const allowedOrigins = [
-    'https://event-management-platform-frontend-bh5b.onrender.com' || 'http://localhost:3000',  // Frontend URL
+    'https://event-management-platform-frontend-bh5b.onrender.com/' || 'http://localhost:3000',  // Frontend URL
 ];
 
 app.use(cors({
